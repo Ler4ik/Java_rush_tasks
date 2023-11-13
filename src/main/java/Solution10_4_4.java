bb4f0af200a1dd8589b40c1547d3b76cb2d575da
Kharlova <valvalsan@yandex.ru>
2023-11-14 01:38:21 +0300

    solution 10_4_4-7

---
+++ src/main/java/Solution10_4_4.java
@@ -0,0 +1,10 @@
+public class Solution10_4_4 {
+
+    public static void main(String[] args) {
+        short number = 9;
+        char zero = '0';
+        int nine = (zero + number);
+        System.out.println((char) nine);
+    }
+
+}
---
+++ src/main/java/Solution10_4_5.java
@@ -0,0 +1,10 @@
+public class Solution10_4_5 {
+
+    public static void main(String[] args) {
+        int a = 44;
+        int b = 300;
+        short c = (short) (b - a);
+        System.out.println(c);
+    }
+
+}
