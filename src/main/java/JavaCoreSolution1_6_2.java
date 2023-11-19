078f3299ba294b5cd5214af160fb0e48d8a0e50d
Kharlova <valvalsan@yandex.ru>
2023-11-19 04:13:46 +0300

    solution 10_11_9-13

---
+++ src/main/java/JavaCoreSolution1_6_2.java
@@ -0,0 +1,18 @@
+public class JavaCoreSolution1_6_2 {
+
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+}
